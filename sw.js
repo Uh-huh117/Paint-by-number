8b33761f6b9e3034b40e8f80a5298fe56ee18cb1
Uh huh <uh.huh.frfr@gmail.com>
2026-08-11 09:34:21 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
-const CACHE='PaintByNumbersv1';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{let q=r.clone();caches.open(CACHE).then(x=>x.put(e.request,q));return r}).catch(()=>caches.match('./index.html'))))});
+const CACHE_NAME = 'pbn-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
